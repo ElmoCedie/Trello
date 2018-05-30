6e3b6e588acbee8b8c82958c745f72705545d31d
cedrick95 <36392647+cedrick95@users.noreply.github.com>
2018-05-30 15:55:55 +0800

    Scss implement

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -157,20 +157,25 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             use: [
-              require.resolve('style-loader'),
+              {
+                loader: require.resolve('style-loader'),
+              },
               {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
-                },
+                }
               },
+
+              {
+                loader: require.resolve('sass-loader'),
+              },
+
               {
                 loader: require.resolve('postcss-loader'),
                 options: {
-                  // Necessary for external CSS imports to work
-                  // https://github.com/facebookincubator/create-react-app/issues/2677
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
@@ -179,14 +184,14 @@ module.exports = {
                         '>1%',
                         'last 4 versions',
                         'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
+                        'not ie < 9',
                       ],
                       flexbox: 'no-2009',
                     }),
                   ],
                 },
               },
-            ],
+            ]
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
@@ -260,73 +265,6 @@ module.exports = {
     hints: false,
   },
 
-  {
-  test: /\.css$/,
-  use: [
-    {
-      loader: require.resolve('style-loader'),
-    },
-    {
-      loader: require.resolve('css-loader'),
-      options: {
-        importLoaders: 1,
-      }
-    },
-    {
-      loader: require.resolve('postcss-loader'),
-      options: {
-        ident: 'postcss',
-        plugins: () => [
-          require('postcss-flexbugs-fixes'),
-          autoprefixer({
-            browsers: [
-              '>1%',
-              'last 4 versions',
-              'Firefox ESR',
-              'not ie < 9',
-            ],
-            flexbox: 'no-2009',
-          }),
-        ],
-      },
-    },
-  ]
-},
-{
-  test: /\.scss$/,
-  use: [
-    {
-      loader: require.resolve('style-loader'),
-    },
-    {
-      loader: require.resolve('css-loader'),
-      options: {
-        importLoaders: 1,
-      }
-    },
 
-    {
-      loader: require.resolve('sass-loader'),
-    },
 
-    {
-      loader: require.resolve('postcss-loader'),
-      options: {
-        ident: 'postcss',
-        plugins: () => [
-          require('postcss-flexbugs-fixes'),
-          autoprefixer({
-            browsers: [
-              '>1%',
-              'last 4 versions',
-              'Firefox ESR',
-              'not ie < 9',
-            ],
-            flexbox: 'no-2009',
-          }),
-        ],
-      },
-    },
-  ]
-},
 };
